6c54468a84cd068a92f5779fc69a03c18ff26468
Patrick Shushereba <pshushereba@gmail.com>
2019-08-10 11:44:18 -0700

    Added Save feature stretch goal.

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -23,10 +23,10 @@ const Movie = (props) => {
   },[]);
   
   // Uncomment this only when you have moved on to the stretch goals
-  // const saveMovie = () => {
-  //   const addToSavedList = props.addToSavedList;
-  //   addToSavedList(movie)
-  // }
+  const saveMovie = () => {
+    const addToSavedList = props.addToSavedList;
+    addToSavedList(movie);
+  }
 
   if (!movie) {
     return <div>Loading movie information...</div>;
@@ -40,7 +40,8 @@ const Movie = (props) => {
         director={director} 
         metascore={metascore} 
         stars={stars} />
-      <div className="save-button">Save</div>
+      <div className="save-button"
+        onClick={saveMovie} >Save</div>
     </div>
   );
 }
